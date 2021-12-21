7ada68aada276f95d7a6a2c43d759d13b55bd926
matheusBrant <matheusbrantgo@gmail.com>
2021-12-21 12:49:45 -0300

    ajustando parametros

--- cloud/consumidores/modelClassificacaoSvm.py
+++ cloud/consumidores/modelClassificacaoSvm.py
@@ -4,6 +4,8 @@ from sklearn.svm import SVC
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
 from csv import reader
 from sklearn import metrics
 import pandas as pd
@@ -16,14 +18,14 @@ dataset_forestfire = pd.read_csv('../dados/dadosClassificacao/forest_fire_classi
 dataset_forestfire= dataset_forestfire.drop(['area'],axis=1) #para dropar a area
 target = dataset_forestfire.pop('fire')
 
-X_train, X_test, y_train, y_test = train_test_split(dataset_forestfire, target, test_size=0.28,random_state=42) # 70% training and 30% test
+X_train, X_test, y_train, y_test = train_test_split(dataset_forestfire, target, test_size=0.28,random_state=109) # 70% training and 30% test
 
-svc = svm.SVC(kernel ='linear', gamma = 10)
+svc = svm.SVC(kernel ='rbf', gamma = 10)
 svc.fit(X_train, y_train)
 y_pred  = svc.predict(X_test)
 #print(confusion_matrix(y_test, y_pred))
 print("Acurácia SVM:",round(metrics.accuracy_score(y_test, y_pred)*100, 2),"%")
-
+print("Precision SVM: ", round(recall_score(y_test, y_pred)*100, 2),"%")
 
 svc.fit(dataset_forestfire, target)
 print(svc.predict([[6,5,92.5,121.1,674.4,8.6,25.1,27,4.0,0.0]]))
